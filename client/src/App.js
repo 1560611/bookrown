0c8d6a1992ac28f8b8a2b5dd14a3eca63bb41357
Thanh Tri <tri.nguyen@notabasement.com>
2018-09-13 13:37:59 +0700

    Init HomePage & LoginPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 
+// Components
+import HomePage from './components/pages/HomePage'
+import LoginPage from './components/pages/LoginPage'
+
 const App = () => (
     <div className="App">
-        <h1>Hello World!</h1>
+        <h1>Bookworm!</h1>
         <div>
-            
+            <Route path="/" exact component={HomePage} />
+            <Route path="/login" exact component={LoginPage} />
         </div>
     </div>
 )
